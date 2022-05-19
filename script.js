80de1a813159b04fd7d457d75a783ccb2cc73e3c
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-05-19 16:20:10 +0900

    Add scroll function

--- script.js
+++ script.js
@@ -48,3 +48,56 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
+
+message.style.backgroundColor = "#37383d"
+message.style.width = "120%";
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+  // console.log("Current scroll(X/Y)", window.pageXOffset, window.pageYOffset);
+  // console.log("height/width viewport",
+  // document.documentElement.clientHeight,
+  // document.documentElement.clientWidth
+  // );
+  // window.scrollTo({
+  // left: s1coords.left + window.pageXOffset,
+  // top: s1coords.top + window.pageYOffset,
+  // behavior: "smooth",
+  // });
+
+  section1.scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+
+// console.log(getComputedStyle(message).height);
+
+// message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 40 + "px";
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered')
+
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
+
+// logo.alt = "Beautiful minimalist logo"
+// logo.setAttribute('company', "Bankist")
+// console.log(logo.getAttribute('src'));
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute("href"));
+
+// console.log(logo.dataset.versionNumber);
+
+// logo.classList.add("c", "j");
+// logo.classList.remove("c", "j");
+// logo.classList.toggle("c");
+// logo.classList.contains("c");
+// logo.className = "jonas";
