826ae780fb0bb9a4c08566fee545a49c9a20a7c7
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-05-21 20:40:24 +0900

    Add revealing elements on scroll

--- script.js
+++ script.js
@@ -164,7 +164,28 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 
 headerObserver.observe(header);
 
+const allSections = document.querySelectorAll('.section')
 
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden')
+  observer.unobserve(entry.target);
+
+}
+
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
 
 // const h1 = document.querySelector('h1');
 // 
